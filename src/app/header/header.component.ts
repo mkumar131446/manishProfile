62970411419a8c43483fa35504ef09ba54f2f9fd
Kumar <mkumar@medline.com>
2023-09-26 14:30:59 -0500

    updated with employer and resume, project and role

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  activeTab: string = 'aboutme';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.setActiveTab(this.router.url);
+  }
+
+  setActiveTab(route: string) {
+    this.router.navigate([route]);
+    if (route.endsWith('skills')) {
+      this.activeTab = 'skills';
+    } else if (route.endsWith('experience')) {
+      this.activeTab = 'experience';
+    }else if (route.endsWith('projects')) {
+      this.activeTab = 'projects';
+    } else {
+      this.activeTab = 'aboutme';
+    }
   }
 
 }
